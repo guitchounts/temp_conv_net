62863c9f48c12692e8060aa91340d4d54379c69b
Guitchounts <gg42@bu.edu>
2017-12-01 17:08:01 -0500

    nonzero

--- decode_infra_all_lfp.py
+++ decode_infra_all_lfp.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         'lr' : 0.0005,
         'kernel' : 2,
         'nb_filter' : 5,
-        'window' : 60,
+        'window' : 100,
         'offset' : 10,
         'nb_test' : 1,
         'nb_trains' : 1,
