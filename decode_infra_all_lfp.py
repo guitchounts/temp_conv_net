8d48812e5a325acafbee6fddc8433bf9718bbf81
Guitchounts <gg42@bu.edu>
2018-09-03 11:07:58 -0400

    unhardcoding

--- decode_infra_all_lfp.py
+++ decode_infra_all_lfp.py
@@ -28,7 +28,7 @@ def filter(ephys,freq_range,filt_order = 4,filt_type='bandpass',fs=10.):
 
 def get_head_stop(head_data): ## head_data.shape = e.g. (1000000, 4)
     all_diffs = []
-    head_names = range(4) #['ox','oy','oz','ax','ay','az']
+    head_names = range(head_data.shape[1])  #['ox','oy','oz','ax','ay','az']
     for head_name in head_names:
         all_diffs.append(np.where(np.diff(head_data[:,head_name],10) == 0 )[0])
 
